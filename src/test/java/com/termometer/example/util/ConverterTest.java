ee3ba74da922313ea1999d21611bddaeb3731796
RodrigFallas <86426662+RodrigFallas@users.noreply.github.com>
2023-06-15 12:10:12 -0600

    Update ConverterTest.java

--- src/test/java/com/termometer/example/util/ConverterTest.java
+++ src/test/java/com/termometer/example/util/ConverterTest.java
@@ -11,7 +11,7 @@ public class ConverterTest {
     @Test
     public void shouldReturnCelsiusWhenFarenheitIsInput() {
         double farenheitInput = 75;
-        double expectedCelsiusResult = 24;
+        double expectedCelsiusResult = 23.88;
 
         assertEquals(expectedCelsiusResult,
                 converter.convertFarenheitCelsius(farenheitInput), 0.02);
